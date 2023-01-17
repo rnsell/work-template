ccfb0972b3ce94fde0bf4626aa6bc57da4288b58
Robert Sell <robertsell21@gmail.com>
2023-01-17 18:06:23 -0500

    initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+import "/styles/globals.css";
+
+import { ThemeProvider } from "@material-tailwind/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
